1c7d226235b3290f698589f91c7dbda7d9812aeb
chenchu2612 <chenchumahesh2412@gmail.com>
2022-11-08 10:51:49 +0530

    morning 10 programms

--- list.py
+++ list.py
@@ -1,2 +1,178 @@
-a=[1,2,3,4]
-print(a)
+# w3 resource programmes
+
+# 1. Write a Python program to sum all the items in a list.
+""""
+def sum_list(items):
+    sum = 0
+    for i in items:
+        sum += i
+    return sum
+
+
+print(sum_list([1, 2, 3]))
+
+"""
+"""
+# 2. Write a Python program to multiply all the items in a list.
+
+
+def mul_list(items):
+    mul = 1
+    for i in items:
+        mul *= i
+    return mul
+
+
+print(mul_list([1, 2]))
+"""
+
+# 3. Write a Python program to get the largest number from a list.
+"""
+def max_num_in_list(items):
+    max = items[0]   # we have to initialize the value for "max" for that we have to take first value in list 
+    for i in items: # compare each value in list with "max" value
+        i > max
+        max = i  "if value is greather_than max then .... "max" then max value is that value 
+    return max
+
+
+print(max_num_in_list([1, 0, 90]))
+"""
+
+# 4.Write a Python program to get the smallest number from a list
+
+"""
+def smallest_num_list(items):
+    min = items[0]
+    for i in items:
+        if i<min:          # this is same as max but we have to write lessthen symbole instead of greateherthen 
+            min = i
+    return min
+
+
+print(smallest_num_list([1, -2, 3]))
+"""
+
+# 5. Write a Python program to count the number of strings where the string length is 2 or more and the first and
+# last character are same from a given list of strings.
+# Sample List : ['abc', 'xyz', 'aba', '1221']
+# Expected Result : 2
+
+"""
+def count_items(items):
+    count = 0
+    for i in items:
+        if len(i) >= 2 and i[0] == i[-1]:
+            count += 1
+    return count
+
+
+print(count_items(['ab', 'xyz', 'abc', '1222']))
+"""
+
+# 6.Write a Python program to get a list, sorted in increasing order by the last element in each tuple
+# from a given list of non-empty tuples. Go to the editor
+# Sample List : [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+# Expected Result : [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
+"""
+
+# sorted increasing order means ascendeing order
+def last(n): return n[-1]   # we have to create a function for the position
+
+
+def sort_list(tuples):   # for tuples sorted is best
+    return sorted(tuples, reverse = True, key=last)
+
+
+print(sort_list([(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]))
+"""
+
+
+# 7. Write a Python program to remove duplicates from a list.
+"""
+def remove_dup(items):
+    new_list = []
+    for i in items:
+        if i not in new_list:
+            new_list.append(i)
+    return new_list
+
+
+print(remove_dup([1, 2, 'a', 3, 1, 3, 'a']))
+"""
+
+#  by using list comprehension method
+
+"""
+def remove_dup_comp(items):
+    l = []   # initialise emepty list 
+    [l.append(i) for i in items if i not in l]  # we are using list comprehension
+    return l
+
+
+print(remove_dup_comp([1, 2, 3, 1]))
+"""
+# 8. Write a Python program to check a list is empty or not.
+
+"""
+def emepty_not(items):
+    if len(items) != 0:
+        print("list is not emepty")
+    else:
+        print("list is emepty")
+
+emepty_not([1])
+"""
+
+# 9.Write a Python program to clone or copy a list.
+
+# copiclone
+
+# copy and cloning both are same we can do those with copy function and by using slice operator
+"""
+new_list = list(oid_list)   # by using list function
+        or
+new_list = old_list.copy()  # by using copy method
+        or
+new_list = old_list[:]    # by using slice operator    
+"""
+
+# 10.Write a Python program to find the list of words that are longer than n from a given list of words.
+
+# list lo vunde words length anedi ichina number kante ekkuva vundali.
+
+"""
+def new_word_list(n, items):
+    new_list = []
+    [new_list.append(i) for i in items if len(i) > n]
+    return new_list
+
+
+print(new_word_list(4, ["apple", "ant", "ball", "cat"]))
+"""
+
+# model: 2 -- incase if they give string of word line
+
+"""
+def new_list_words(n, items):
+    st_to_list = items.split() # splitting the string into list (by default split function splits based on spaces)
+    new_list = []
+    [new_list.append(i) for i in st_to_list if len(i) > n]
+    return new_list
+
+
+print(new_list_words(4, "The quick brown fox jumps over the lazy dog"))
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+

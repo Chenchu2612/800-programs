ad88b934a8dd12c7832af88e449c0ad2aff58431
Chenchu@2612 <chenchumahesh2412@gmail.com>
2022-11-22 09:58:09 +0530

    adding functions

--- functions.py
+++ functions.py
@@ -137,3 +137,112 @@ def create_unique_list(list_obj):
 print(create_unique_list(eval(input("Enter the list: "))))
 
 """
+"""
+# Model : checking prime or not
+
+# 9. Write a Python function that takes a number as a parameter and check the number is prime or not.
+
+# Note : A prime number (or a prime) is a natural number greater than 1 and that has no positive divisors other than 1 and itself.
+
+# Explanation :
+# 1. number 1 kanna ekkuvaga vundali.
+# 2. ichina number anedi shound not divisible by (2, number-1)
+# 3. For loop with else>>>>>> For, else, break statement
+
+
+def checking_prime(number):
+    if number > 1 :
+        for x in range(2, number):
+            if number % x == 0:
+                print('It is not a prime number')
+                break
+        else:
+            print('It is a prime number')
+    else:
+        print('It is not prime number')
+
+
+checking_prime(int(input("Enter the number :")))
+
+"""
+
+"""
+# Model : Extract only even numbers
+
+# 10. Write a Python program to print the even numbers from a given list.
+Sample_List = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# Expected Result : [2, 4, 6, 8]
+
+
+def even_numbers(list_obj):
+    return [number for number in list_obj if number % 2 ==0]  # taking only even numbers.
+
+
+print(even_numbers(Sample_List))
+
+"""
+"""
+# Model : Finding perfect number.
+
+# 11.Write a Python function to check whether a number is perfect or not.
+
+# According to Wikipedia : In number theory, a perfect number is a positive integer that is equal to the sum of its proper positive divisors,
+# that is, the sum of its positive divisors excluding the number itself (also known as its aliquot sum).
+# Equivalently, a perfect number is a number that is half the sum of all of its positive divisors (including itself).
+
+# Example : The first perfect number is 6, because 1, 2, and 3 are its proper positive divisors, and 1 + 2 + 3 = 6.
+# Equivalently, the number 6 is equal to half the sum of all its positive divisors: ( 1 + 2 + 3 + 6 ) / 2 = 6.
+# The next perfect number is 28 = 1 + 2 + 4 + 7 + 14. This is followed by the perfect numbers 496 and 8128.
+
+
+def finding_perfect_or_not(number):
+    sum = 0
+    for num in range(1, number):
+        if number % num == 0:
+            sum += num
+    if sum == number:
+        return '{} >>>> It is a perfect_number'.format(number)
+    return '{} >>>> It is not a perfect_number'.format(number)
+
+
+print(finding_perfect_or_not(int(input("Enter the perfect number :"))))
+"""
+"""
+# Model : Polindrome
+
+# 12. Write a Python function that checks whether a passed string is palindrome or not.
+# Note: A palindrome is a word, phrase, or sequence that reads the same backward as forward, e.g., madam or nurses run.
+
+# Explanation : 1. ichina string nunchi emepty string's ni remove cheyyali
+#               2. string ni reverse cheyyali.
+#               3. reverse chesina string ni and emepty string's ni remove chesina string ni compare cheyyali. redu okate iethe True ledante False
+
+
+def check_polindrome(string):
+    new_string = ''.join([ch for ch in string if ch != " "])
+    reversed_string = ''
+    for ch in new_string:
+        reversed_string = ch + reversed_string
+    if reversed_string == new_string :
+        return '{} It is polindrome'.format(string)
+    return '{} It is not a polindrome'.format(string)
+
+
+print(check_polindrome(input("Enter the number :")))
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

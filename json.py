d07ed3331845b9ebcd7146028a77f442988fde05
Chenchu@2612 <chenchumahesh2412@gmail.com>
2022-11-27 13:07:41 +0530

    add

---
+++ json.py
@@ -0,0 +1,95 @@
+# Serialization: the process of converting an object from python supported form into network supported or file supported form is called as serialization
+# this conversion process by default considered as serializtion.
+import json
+
+# Deserialization : the process of converting an object from file supported form or network supported form into python object is called as deserialization
+
+# pickle module is used for serializing and deserializing.
+
+
+
+# what is json ?
+
+# ans):  json- java script object notation ... the most commonly used message formate for applications irrespective of language ...
+
+# any programming language can understand json.
+
+# json is similar to python dictionary objects ....jason contains group of key value pairs.
+
+# i) The biggest specality of json is any programming language can understand the json
+
+# ii) light weight and required less memory to store data
+
+# There are mainly four functions in json :
+
+# a)json.dumps(): converting an python dict object to the json string.  (serialization to a string (Here string is nothing but json string))
+
+# coding : json.dumps(dict_obj, indent = num, separator = (", ", ": "), sort_keys = True/False)
+
+# b) json.dump(): converting an python dict object to a json string and write that json to provided jason file. (serialization to a file)
+
+# code : with open('emp.json', 'w+') as f :
+#             json.dump(python_dict_object, f, indent=num, separator= (", ", ": "), sort_keys = True/False)
+
+# manam koasari aliyas name ichinappudu aa entire code lo danne vadali...ex: f
+
+
+# c) json.loads(): converting an json string to an python dict obj ... (deserialization from string)
+
+# code : dict_obj=json.loads(json_str)
+
+# for key, value in dict_obj.items():
+#         print('{},{}'.format(key,value)
+
+# d) json.load(): Reading json string from a file and converting to python dict_object (deserialization from a file)
+# with open('emp.json', 'r+') as f:
+#         dict_obj = json.load(f)
+
+# for key, value in dict_obj.items():
+#     print('{},{}'.format(key, value)
+
+
+# json is a syntax for sorting and exchanging data, json text is written in Javascript object notation.
+
+
+# if you want to work with json in python we have to import json module. (import json)
+
+# python to json :- dumps ::: json.dump()
+
+# json to python :- load ::: json.load()
+
+# when you convert python to json  python objects are converted into json(javascript) objects.
+
+# trick : mfjson load chestunnadu..... pd...
+
+
+# json.dumps(python_obj, indent = num, separators = (". ", " = ") , sort_keys = True)
+
+
+# indent = number of indentations.
+# separators are used to separate each object and the each key value. default is (", ", ": ") it means each object is separated comma and space---
+# and each key value pair is separated by colon (:) and space
+# sort_keys is used to sort the keys (sort_keys= True or False) ...the sort_keys parameter to specify if the result should be sorted or not.
+
+
+
+
+
+# Questions:
+
+# Model : JSON data to python object.
+
+# 1. Write a Python program to convert JSON data to Python object.
+
+
+
+
+
+
+
+
+
+
+
+
+
